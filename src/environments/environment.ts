3b0cd8a833a6ac6d0ae25109053dc72cc6c94d27
Serah wakaba <serahwakaba9@gmail.com>
2021-05-24 00:36:25 +0300

    main folder

---
+++ src/environments/environment.ts
@@ -0,0 +1,20 @@
+import { Key } from "protractor";
+
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  // api_Key:"c63d7470f4b5759fe15a569f9b2fc2368a355c86 "
+  api_Key: "7377647d064385d967e03bfbffdc62983c19341a"
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
